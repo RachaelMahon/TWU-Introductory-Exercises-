f60e861ed7e7c36e0a37448f1db96067870396c2
RachaelMahon <rachmahon@gmail.com>
2017-04-10 19:19:51 +0100

    Fizzbuzz is working in reverse order of integers

--- src/Run_Exercises.java
+++ src/Run_Exercises.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by rachaelmahon on 10/04/2017.
  */
@@ -5,5 +6,9 @@ public class Run_Exercises {
     public static void main(String[] args) {
         Exercise_1 run_exercise_1 = new Exercise_1();
         run_exercise_1.Run_Exercise_1();
+
+        Fizzbuzz run_fizzbuzz = new Fizzbuzz();
+        run_fizzbuzz.run_Fizzbuzz(30);
+
     }
 }
